2f7481fdf070b18d8098a8ee02799bcfeb5836f5
AngeliM96 <matiasangeli@gmail.com>
2021-03-21 22:12:05 -0300

    Codigo adaptado al Nano y RGB

---
+++ nano_calib_sensor/funciones.ino
@@ -0,0 +1,195 @@
+//-----------------Alarma----------------------------
+void alarma(int veces, int duracionNota) {
+  for(int i=0; i<veces; i++)
+  {
+    tone(pinBuzzer, 523, duracionNota);   // Hago sonar el buzzer, 523 corresponde a la nota C5
+    delay(duracionNota);                  // Espero lo que dura la nota
+    noTone(pinBuzzer);                    // Silencio el buzzer
+    delay(duracionNota);                  // Delay entre alarmas
+  }
+}
+
+// Creacion del simbolo de numeral para ser utilizado en el display i2c
+byte numeral[] = {
+  B11100,
+  B10100,
+  B11100,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+//-----------Print-por-display----------------------
+void displayPrint(int posicion, int linea, String texto) {
+  display.setCursor(posicion, linea);       // Ubicamos el cursor en la posicion y linea deseada
+  display.print(texto);                     // Escribe primera linea del cartel
+}
+void imprimirCO2(int co2ppm) {
+  // Print por serial  
+  Serial.print("CO2: " + String(co2ppm) + "ppm \n");  // Escribe CO2
+  // Print por display
+  // Muestra medición de CO2  
+  //display.clear();                                  // Borra pantalla
+  displayPrint(0, 1, "          ");                   // Simula un clear solo del renglon inferior
+  // Print CO2
+  displayPrint(0, 1, "CO2: " + String(co2ppm) + "ppm");   
+  logoUNAHUR();
+}
+void logoUNAHUR() {
+  byte UNAHUR1[] = {
+    B11100,
+    B11110,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B01111,
+    B00111
+  };
+  byte UNAHUR2[] = {
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111
+  };
+  byte UNAHUR3[] = {
+    B00111,
+    B01111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11110,
+    B11100
+  };
+  display.createChar(0, UNAHUR1);
+  display.createChar(1, UNAHUR2);
+  display.createChar(2, UNAHUR3);
+  display.setCursor(13, 0);       // Ubicamos el cursor en la posicion y linea deseada
+  display.write(0);
+  display.setCursor(14, 0);       // Ubicamos el cursor en la posicion y linea deseada
+  display.write(1);
+  display.setCursor(15, 0);       // Ubicamos el cursor en la posicion y linea deseada
+  display.write(2);
+  display.setCursor(13, 1);       // Ubicamos el cursor en la posicion y linea deseada
+  display.write(2);
+  display.setCursor(14, 1);       // Ubicamos el cursor en la posicion y linea deseada
+  display.write(1);
+  display.setCursor(15, 1);       // Ubicamos el cursor en la posicion y linea deseada
+  display.write(0);
+}
+//-----------Calibración----------------------
+void calibrar()
+{
+  const long segundosEspera = 1800;      // Cantidad de segundos a esperar
+  long segundosPasados = 0;              // Cuenta segundos
+  // Print por serial
+  Serial.print("COMIENZA CALIBRACION \n"); // Notificamos que empezó el proceso de calibración
+  // Print por display
+  display.clear();
+  displayPrint(0, 0, "COMIENZA");
+  displayPrint(0, 1, "CALIBRACION");
+  rgb('b');
+  delay(10000); // Espera 10 segundos
+ 
+  while(segundosPasados <= segundosEspera) {                      // Espera media hora
+    if (++segundosPasados % 60 == 0) {                            // Si los segundos pasados son múltiplo de 60
+      // Print por serial
+      Serial.print(String(segundosPasados / 60) + " minutos \n"); // Cada minuto muestra el valor 
+      Serial.print("CO2: " + String(sensor.getPPM()) + "ppm \n"); // Escribe CO2
+      // Print por display
+      display.clear();                                            // Borra pantalla  
+      displayPrint(0, 0, String(segundosPasados / 60));           // Escribimos los minutos pasados
+      displayPrint(7, 0, "minutos");
+      displayPrint(0, 1, "CO2: ");                                // Escribimos el CO2
+      displayPrint(8, 1, String(sensor.getPPM()));
+      displayPrint(12, 1, "ppm");
+    }
+    else {
+      // Print por display
+      display.clear();                                            // Borra pantalla  
+      displayPrint(0, 0, "CALIBRANDO");
+      displayPrint(0, 1, String(segundosPasados / 60));           // Escribimos los minutos pasados
+      displayPrint(7, 1, "minutos");
+    }
+    delay(1000); // Espera 1 segundo
+  }
+  sensor.calibrateZero();               // Calibra
+  // Print por serial
+  Serial.print("PRIMERA CALIBRACION \n");
+  // Print por display
+  display.clear();                      // Limpio pantalla
+  displayPrint(0, 0, "PRIMERA");       
+  displayPrint(0, 1, "CALIBRACION");     
+  alarma(1, 250);                       // Avisamos que terminó la primera calibración
+  delay(60000);                         // Espera 1 minuto
+  sensor.calibrateZero();               // Calibra por segunda vez por las dudas
+  // Print por serial
+  Serial.print("SEGUNDA CALIBRACION \n");
+  // Print por display
+  display.clear();                      // Limpio pantalla     
+  displayPrint(0, 0, "SEGUNDA");        
+  displayPrint(0, 1, "CALIBRACION");    
+  alarma(1, 250);                       // Avisamos que terminó la segunda calibración
+  delay(10000); // Espera 10 segundos
+  // Print por serial
+  Serial.print("CALIBRACION TERMINADA \n");
+  // Print por display
+  display.clear(); // borra pantalla  
+  displayPrint(0, 0, "CALIBRACION");       
+  displayPrint(0, 1, "TERMINADA");    
+  alarma(5, 250);                       // Avisamos que finalizó el proceso de calibración entero
+  rgb('g');
+  delay(10000); // Espera 10 segundos 
+}
+
+//------------Scrolling-Text-----------------------
+#define STR_LEN 12                                                      // Largo del texto
+char str_to_print[STR_LEN]={'A','i','r','e',' ','N','u','e','v','o'};   // String separado en caracteres
+
+void scrollingText(uint8_t scrolled_by) {
+  for (uint8_t i=0;i<11;i++) {
+    display.setCursor(i,0);
+    if (scrolled_by>=11) scrolled_by=0;
+    if (scrolled_by<10) display.print(str_to_print[scrolled_by]);
+    else display.print(' ');
+    scrolled_by++;
+  }
+}
+
+void aireNuevo() {
+  for (uint8_t i=0;i<STR_LEN;i++) {
+    scrollingText(i);
+    delay(500);
+  }
+}
+//-----------Led-RGB----------------------
+void rgb(char color) {
+  switch (color) {
+    case 'r':
+      digitalWrite(pinLedR, 0);
+      digitalWrite(pinLedG, 255);
+      digitalWrite(pinLedB, 255);
+      break;
+    case 'g':
+      digitalWrite(pinLedR, 255);
+      digitalWrite(pinLedG, 0);
+      digitalWrite(pinLedB, 255);
+      break;
+    case 'y':
+      digitalWrite(pinLedR, 0);
+      digitalWrite(pinLedG, 0);
+      digitalWrite(pinLedB, 255);
+      break;
+    case 'b':
+      digitalWrite(pinLedR, 255);
+      digitalWrite(pinLedG, 255);
+      digitalWrite(pinLedB, 0);
+      break;
+  }
+}
---
+++ nano_calib_sensor/nano_calib_sensor.ino
@@ -0,0 +1,99 @@
+/*----------------------------------------------------------
+    Medidor de CO2 con placa Arduino Nano, pantalla i2c, led RGB
+  ----------------------------------------------------------*/
+#include <MHZ19_uart.h>     
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+//-----------------Pines----------------------------
+const int rx_pin = 3;     // Serial rx
+const int tx_pin = 4;     // Serial tx
+const int pinLedR = 7;     // Led Rojo
+const int pinLedG = 6;     // Led Verde
+const int pinLedB = 5;     // Led Azul
+const int pinBuzzer = 8;   // Buzzer
+const int pinCalib = 2;    // Pulsador
+const String numeroSerie = "0001"; 
+//--------------------------------------------------
+long loops = 0;                         // Contamos las veces que se ejecutó el loop
+MHZ19_uart sensor;                      // Creo el objeto del sensor
+LiquidCrystal_I2C display(0x27,16,2);   // Creo el objeto display dirección 0x27 y 16 columnas x 2 filas
+//-----------------Setup----------------------------
+void setup() {
+  pinMode(pinLedR, OUTPUT);               // Inicia LED rojo
+  pinMode(pinLedG, OUTPUT);               // Inicia LED verde
+  pinMode(pinLedB, OUTPUT);               // Inicia LED azul
+  pinMode(pinBuzzer, OUTPUT);            // Inicia Buzzer
+  pinMode(pinCalib, INPUT_PULLUP);       // Entrada pulsador para calibrar, setteada como pullup para poder conectar pulsador sin poner resistencia adicional
+  Serial.begin(9600);                    // Iniciamos el serial
+  display.begin();                       // Inicio el display            
+  display.clear();                       // Limpio la pantalla 
+  display.backlight();                   // Prendo la backlight
+  alarma(1, 250);
+  rgb('b');
+  // Imprimimos un mensaje mostrando el número de serie del equipo y un cartel de que el equipo se está iniciando
+  // Print por serial
+  Serial.print("N° de serie " + numeroSerie + "\n");
+  Serial.print("INICIANDO \n");
+  // Print por display
+  // Print numero serie
+  displayPrint(0, 0, "N/S: " + numeroSerie);
+  displayPrint(0, 1, "INICIANDO");       
+  logoUNAHUR();                          // Dibuja el logo de la UNAHUR en el lado derecho de la pantalla.
+  delay(10000);                          // Esperamos 10 segundos
+  display.clear();                       // Limpio la pantalla
+  // Calentamos 
+  sensor.begin(rx_pin, tx_pin);          // Setteamos los pines y iniciamos el sensor
+  sensor.setAutoCalibration(false);      //Se setea la autocalibracion en falso para poder calibrar cuando se desee        
+  // Avisamos que el dispositivo se está calentando y que se debe esperar                       
+  // Print por serial
+  Serial.print("Calentando, espere 1 minuto \n");
+  // Print por display
+  displayPrint(0, 0, "Calentando");       
+  displayPrint(0, 1, "Espere 1 minuto"); 
+  delay(60000);                           // Esperamos 1 minuto
+  display.clear();                        // Limpio la pantalla
+  alarma(3, 250);                         // Alarma indicando que terminó el calentamiento
+  rgb('g');
+}
+
+void loop() {
+  if (digitalRead(pinCalib) == LOW) {           // Si el pulsador está presionado, se ejecuta la calibración
+    alarma(1, 250);
+    calibrar();
+  }
+  if(++loops % 30 == 0) {                       // Si loops es múltiplo de 30 
+                                                // Imprimimos por pantalla un mensaje presentando el dispositivo
+    // Print por serial
+    Serial.print("AireNuevo UNAHUR \n"); 
+    Serial.print("MEDIDOR de CO2 \n");  
+    // Print por display
+    display.clear();                            // Limpio la pantalla
+    displayPrint(0, 0, "AireNuevo UNAHUR");     
+    displayPrint(0, 1, "MEDIDOR de CO2");       
+    delay(5000);
+    loops = 0;                                  // Reinicio la cuenta
+  } 
+  display.clear();
+  displayPrint(0, 0, "Aire Nuevo");
+  //  Emite una alarma en función del resultado
+  while(sensor.getPPM() >= 1200) {              // Acá todavía no podemos usar una variable porque tiene que ser 
+    alarma(1, 250);                             // la medición en tiempo real para que suene constantemente
+    rgb('r'); 
+    imprimirCO2(sensor.getPPM());
+  }
+  int co2ppm = sensor.getPPM();                 // Guardamos el CO2 en una variable
+  imprimirCO2(co2ppm);
+  if(co2ppm >= 1000){
+    alarma(4, 500);
+    rgb('r');                         
+  } 
+  else if(co2ppm >= 800){
+    alarma(2, 1000);
+    rgb('y');
+  }
+  else if(co2ppm < 800){
+    rgb('g');
+  }
+  aireNuevo();                                  // Imprimimos un cartel que se va moviendo de derecha a izquierda con la leyenda "Aire Nuevo"
+  delay(5000);                                  // Demora 10 seg entre mediciones, 5 segundos pasan con el mensaje de Aire Nuevo, y 5 se esperan
+}
